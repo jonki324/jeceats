7601e3f09e6d49a0ba55c99e3ef970940dc87857
jonki324 <jonki324@gmail.com>
2021-08-21 17:08:48 +0900

    ResponseDTO作成

---
+++ src/main/java/resource/response/ImagePresignedUrlResponse.java
@@ -0,0 +1,26 @@
+package resource.response;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Accessors(chain = true)
+@EqualsAndHashCode
+@ToString
+public class ImagePresignedUrlResponse implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String presignedObjectUrl;
+
+    private String objectName;
+}
